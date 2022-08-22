4a7daae354ba8588f21e623923e9312c44d7d396
Lerner Saúl Morales Gómez <lernersaul265@gmail.com>
2022-08-22 17:33:35 -0600

    Prueba de Equipo.

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Aqui comineza todo, ahora solo tenemos que idear las bases de nuestra tarea.");
+		
+	}
+
+}
